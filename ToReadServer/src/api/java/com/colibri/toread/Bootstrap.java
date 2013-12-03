9671d2766aa712380242ddb583b792fdc2d0e00a
James <James@192.168.1.3>
2013-12-03 22:24:07 +0000

    Bootstrapping

---
+++ ToReadServer/src/api/java/com/colibri/toread/Bootstrap.java
@@ -0,0 +1,31 @@
+package com.colibri.toread;
+
+import org.restlet.Component;
+import org.restlet.Server;
+import org.restlet.data.Parameter;
+import org.restlet.data.Protocol;
+import org.restlet.util.Series;
+
+public class Bootstrap {
+		public static void main(String[] args) throws Exception {
+
+			//Create a new Component.
+			Component component = new Component();
+			
+			//Allow file handling
+			component.getClients().add(Protocol.FILE);
+			
+			//Create a new HTTPS server listening on port 2620.
+			Server server = component.getServers().add(Protocol.HTTPS, 2610);
+			Series<Parameter> params = server.getContext().getParameters(); 
+			
+			params.add("sslContextFactory", "org.restlet.ext.ssl.PkixSslContextFactory");
+			params.add("keystorePath", "server.jks");
+			params.add("keystorePassword", "password");
+			params.add("keyPassword", "password");
+			params.add("keystoreType", "JKS");
+
+			//Start the component.
+			component.start();
+		}
+}
