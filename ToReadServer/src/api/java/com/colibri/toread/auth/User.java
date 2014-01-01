262659efc0cd53663d39537f1426a2b4056c2b9b
James <James@jamess-air.home>
2014-01-01 22:09:24 +0000

    /

--- ToReadServer/src/api/java/com/colibri/toread/auth/User.java
+++ ToReadServer/src/api/java/com/colibri/toread/auth/User.java
@@ -1,5 +1,7 @@
 package com.colibri.toread.auth;
 
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -8,9 +10,71 @@ import com.colibri.toread.ToReadBaseEntity;
 public class User extends ToReadBaseEntity {
 	private String user_name;
 	private String email_address;
-	private String password;
+	private Password password;
 	private ArrayList<Device> devices = new ArrayList<Device>(); //All of a users registered devices
 	private String first_name;
 	private String last_name;
 	private Date dob;
+	
+	public String getUser_name() {
+		return user_name;
+	}
+	public void setUser_name(String user_name) {
+		this.user_name = user_name;
+	}
+	public String getEmail_address() {
+		return email_address;
+	}
+	public void setEmail_address(String email_address) {
+		this.email_address = email_address;
+	}
+	public boolean validatePassword(String testPassword) {
+		try {
+			return(this.password.validate(testPassword));
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		} catch (InvalidKeySpecException e) {
+			e.printStackTrace();
+		}
+		
+		return false;
+	}
+	public void setPassword(String passwordString) {
+		Password password = new Password();
+		try {
+			password.encrypt(passwordString);
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		} catch (InvalidKeySpecException e) {
+			e.printStackTrace();
+		}
+		
+		this.password = password;
+	}
+	public ArrayList<Device> getDevices() {
+		return devices;
+	}
+	public void setDevices(ArrayList<Device> devices) {
+		this.devices = devices;
+	}
+	public String getFirst_name() {
+		return first_name;
+	}
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+	public String getLast_name() {
+		return last_name;
+	}
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+	public Date getDob() {
+		return dob;
+	}
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
 }
---
+++ ToReadServer/src/api/java/com/colibri/toread/persistence/UserDAO.java
@@ -0,0 +1,13 @@
+package com.colibri.toread.persistence;
+
+import org.bson.types.ObjectId;
+import com.colibri.toread.auth.User;
+import com.google.code.morphia.Morphia;
+import com.google.code.morphia.dao.BasicDAO;
+import com.mongodb.MongoClient;
+
+public class UserDAO extends BasicDAO<User, ObjectId> {
+	public UserDAO( Morphia morphia, MongoClient mongo) {
+		super(mongo, morphia, "bookCollection");
+	}
+}
